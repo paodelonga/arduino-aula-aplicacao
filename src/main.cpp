b0e5a3d01d57cd23f858eadad4791a7bebba8a81
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-06-17 10:18:24 -0300

    god

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-#include <Servo.h>
 #include <math.h>
-
 
 #define PINO_PRIMEIRO_SENSOR A1
 #define PINO_SEGUNDO_SENSOR A2
 #define PINO_TERCEIRO_SENSOR A3
 #define PINO_PUSH_BUTTON A5
 
-double posicao_do_sensor[3] = {25, 50, 85};
+const double POSICAO_DO_SENSOR[3] = {25, 50, 85};
+
 double tempo_do_sensor[4];
 
 // cppcheck-suppress unusedFunction
@@ -17,29 +15,37 @@ void setup() {
   pinMode(PINO_PRIMEIRO_SENSOR, INPUT);
   pinMode(PINO_SEGUNDO_SENSOR, INPUT);
   pinMode(PINO_TERCEIRO_SENSOR, INPUT);
+  pinMode(PINO_PUSH_BUTTON, INPUT);
 
   Serial.begin(115200);
   while (!Serial) {
     delay(250);
   }
-
 }
 
 // cppcheck-suppress unusedFunction
 void loop() {
+  if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
+    while (1) {
+      if (analogRead(PINO_PRIMEIRO_SENSOR) < 600) {
+        tempo_do_sensor[1] = millis();
 
-if(button.pressed()) {
-  while(1) {
-    if (analogRead() < 600) {
-    }
-    while(1) {
-      if (analogRead() < 600) {
+        while (1) {
+          if (analogRead(PINO_SEGUNDO_SENSOR) < 600) {
+            tempo_do_sensor[2] = millis();
+
+            while (1) {
+              if (analogRead(PINO_TERCEIRO_SENSOR) < 600) {
+                tempo_do_sensor[3] = millis();
+
+              }
+            }
+          }
+        }
       }
-      while(1) {
+      if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
+        break;
       }
     }
   }
-}
-
-
 }
