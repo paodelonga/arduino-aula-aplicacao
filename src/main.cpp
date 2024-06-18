b7a76378e219d8bdfb01c1b7b0817273b8f1cb6a
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-06-18 17:26:31 -0300

    babalu

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,59 @@
 #include <Arduino.h>
 
-#define PINO_PRIMEIRO_SENSOR A1
-#define PINO_SEGUNDO_SENSOR A2
-#define PINO_TERCEIRO_SENSOR A3
-#define PINO_PUSH_BUTTON 1
-#define PINO_LED_VERDE 2
-#define PINO_LED_AMARELO 3
+#define PINO_SENSOR A1
+#define PINO_PUSH_BUTTON 2
+#define PINO_LED 3
 
-const double POSICAO_DO_SENSOR[3] = {25, 50, 85};
+double tempo_do_sensor[1];
 
-double tempo_do_sensor[4];
-
-// cppcheck-suppress unusedFunction
 void setup() {
-  pinMode(PINO_PRIMEIRO_SENSOR, INPUT);
-  pinMode(PINO_SEGUNDO_SENSOR, INPUT);
-  pinMode(PINO_TERCEIRO_SENSOR, INPUT);
-
+  pinMode(PINO_SENSOR, INPUT);
   pinMode(PINO_PUSH_BUTTON, INPUT);
-
-  pinMode(PINO_LED_VERDE, OUTPUT);
-  pinMode(PINO_LED_AMARELO, OUTPUT);
+  pinMode(PINO_LED, OUTPUT);
 
   Serial.begin(115200);
   while (!Serial) {
     delay(250);
   }
 
-  digitalWrite(PINO_LED_VERDE, LOW);
-  digitalWrite(PINO_LED_AMARELO, LOW);
+  digitalWrite(PINO_LED, LOW);
 }
 
-// cppcheck-suppress unusedFunction
 void loop() {
-  tempo_do_sensor[0] = millis();
-  if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
-    digitalWrite(PINO_LED_VERDE, HIGH);
-    Serial.println("INICIANDO SENSORES");
-    Serial.println("LIBERE O OBJETO");
+  Serial.println("PRESSIONE O BOTÃO PARA INICIAR A LEITURA");
+  Serial.println("E AGUARDE O LED ACENDER PARA LIBERAR O OBJETO\n");
 
-    while (1) {
-      if (analogRead(PINO_PRIMEIRO_SENSOR) < 600) {
-        tempo_do_sensor[1] = millis() - tempo_do_sensor[0];
-        digitalWrite(PINO_LED_VERDE, LOW);
-        digitalWrite(PINO_LED_AMARELO, HIGH);
-        Serial.println("PRIMEIRO SENSOR INTERROMPIDO");
-
-        while (1) {
-          if (analogRead(PINO_SEGUNDO_SENSOR) < 600) {
-            tempo_do_sensor[2] = millis() - tempo_do_sensor[0];
-            Serial.println("SEGUNDO SENSOR INTERROMPIDO");
-
-            while (1) {
-              if (analogRead(PINO_TERCEIRO_SENSOR) < 600) {
-                tempo_do_sensor[3] = millis() - tempo_do_sensor[0];
-                Serial.println("TERCEIRO SENSOR INTERROMPIDO");
-                return;
-              }
-              digitalWrite(PINO_LED_AMARELO, LOW);
-              Serial.println("LEITURA FINALIZADA");
-            }
-          }
-        }
+  while (1) {
+    if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
+      for (byte i = 5; i > 0; i--) {
+        Serial.println((String) "LIBERE O OBJETO EM: " + i + "s");
+        digitalWrite(PINO_LED, LOW);
+        delay(200);
+        analogWrite(PINO_LED, 524);
+        delay(1000);
       }
-      if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
-        digitalWrite(PINO_LED_VERDE, LOW);
-        return;
+
+      tempo_do_sensor[0] = millis();
+      digitalWrite(PINO_LED, HIGH);
+      Serial.println("\nLEITURA INICIADA");
+      while (1) {
+        if (analogRead(PINO_SENSOR) > 1020) {
+          tempo_do_sensor[1] = millis() - tempo_do_sensor[0];
+
+          digitalWrite(PINO_LED, LOW);
+          Serial.println((String)"OBJETO DETECTADO APOS " + tempo_do_sensor[1] + "ms");
+          Serial.println("LEITURA FINALIZADA\n");
+
+          digitalWrite(PINO_LED, HIGH);
+          delay(800);
+          digitalWrite(PINO_LED, LOW);
+          return;
+        }
+        if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
+          Serial.println("LEITURA INTERROMPIDA\n");
+          digitalWrite(PINO_LED, LOW);
+          return;
+        }
       }
     }
   }
