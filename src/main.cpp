2fb588035ceda8140d3f99715312e081e6dd7934
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-06-19 14:07:25 -0300

    coiote

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,100 @@
 #include <Arduino.h>
 
-#define PINO_SENSOR A1
-#define PINO_PUSH_BUTTON 2
-#define PINO_LED 3
+// Definição dos pinos
+#define PINO_SENSOR = A1;
+#define PINO_PUSH_BUTTON = 2;
+#define PINO_LED = 3;
 
-double tempo_do_sensor[1];
+// Declara variaveis para marcação do horario dos sensores
+unsigned long tempo_inicio_leitura;
+unsigned long tempo_objeto_detectado;
 
 void setup() {
+  // Configuração do modo de atuação dos pinos
   pinMode(PINO_SENSOR, INPUT);
   pinMode(PINO_PUSH_BUTTON, INPUT);
   pinMode(PINO_LED, OUTPUT);
 
+  // Configura a taxa de transferência da comunicação Serial
   Serial.begin(115200);
+
+  // Espera até que a comunicação Serial esteja disponível
   while (!Serial) {
-    delay(250);
   }
 
+  // Envia um sinal LOW para garantir que o LED
+  // esteja desligado antes de iniciar a leitura
   digitalWrite(PINO_LED, LOW);
 }
 
 void loop() {
-  Serial.println("PRESSIONE O BOTÃO PARA INICIAR A LEITURA");
-  Serial.println("E AGUARDE O LED ACENDER PARA LIBERAR O OBJETO\n");
+  // Instruções para o usuário
+Serial.println("===================== INSTRUÇÕES =====================");
+Serial.println("- Pressione o botão para iniciar a leitura.");
+Serial.println("- O LED irá piscar durante 5 segundos com brilho baixo.");
+Serial.println("- Após isso, o LED ficará com brilho alto.");
+Serial.println("- Quando o LED estiver com brilho alto, solte o objeto.");
+Serial.println("- O LED se apagará quando o objeto for detectado.");
+Serial.println("- O tempo de detecção será exibido no monitor serial.");
+Serial.println("=======================================================  ");
 
-  while (1) {
+  // Espera até que o botão seja pressionado
+  while (true) {
     if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
-      for (byte i = 5; i > 0; i--) {
-        Serial.println((String) "LIBERE O OBJETO EM: " + i + "s");
+      /* Contagem regressiva de 5 segundos com o LED piscando.
+        Itera sobre i 5 vezes e utiliza delay
+        para criar um tempo de espera entre cada iteração */
+      for (uint8_t i = 5; i > 0; i--) {
+        Serial.print("O LED PARA DE PISCAR EM: ");
+        Serial.print(i);
+        Serial.println("s");
+
+        // Pisca o LED para cada iteração (segundo).
         digitalWrite(PINO_LED, LOW);
         delay(200);
-        analogWrite(PINO_LED, 524);
-        delay(1000);
+        analogWrite(PINO_LED, 128);
+
+        // Aguarda 800 milissegundos para garantir
+        // que cada iteração leve o total de 1000 milissegundos
+        delay(800);
       }
 
-      tempo_do_sensor[0] = millis();
+      // Registra o tempo de início da leitura
+      tempo_inicio_leitura = millis();
+
+      // Envia um sinal HIGH para ligar o LED
+      // que indica o inicio da leitura
       digitalWrite(PINO_LED, HIGH);
-      Serial.println("\nLEITURA INICIADA");
-      while (1) {
+
+      Serial.println("\nSOLTE O OBJETO.");
+
+      // Loop para monitorar o sensor
+      while (true) {
         if (analogRead(PINO_SENSOR) > 1020) {
-          tempo_do_sensor[1] = millis() - tempo_do_sensor[0];
+          // Calcula o tempo desde o início da leitura até a detecção do objeto
+          unsigned long tempo_objeto_detectado = millis() - tempo_inicio_leitura;
 
+          // Envia um sinal LOW para desligar o LED
+          // indicando detecção do objeto
           digitalWrite(PINO_LED, LOW);
-          Serial.println((String)"OBJETO DETECTADO APOS " + tempo_do_sensor[1] + "ms");
-          Serial.println("LEITURA FINALIZADA\n");
 
-          digitalWrite(PINO_LED, HIGH);
-          delay(800);
-          digitalWrite(PINO_LED, LOW);
+          // Exibe uma mensagem contendo o tempo de detecção do sensor
+          Serial.print("OBJETO DETECTADO APOS ");
+          Serial.print(tempo_objeto_detectado);
+          Serial.println("ms");
+          Serial.println("LEITURA FINALIZADA\n\n");
+
+          // Retorna para iniciar o loop novamente
           return;
         }
+        // Verifica se o botão foi pressionado para interromper a leitura
         if (digitalRead(PINO_PUSH_BUTTON) == HIGH) {
-          Serial.println("LEITURA INTERROMPIDA\n");
+          Serial.println("LEITURA INTERROMPIDA\n\n");
+
           digitalWrite(PINO_LED, LOW);
+
+          // Retorna para iniciar o loop novamente
+          delay(500);
           return;
         }
       }
